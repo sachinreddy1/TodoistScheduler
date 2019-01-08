78bfb17403019fb7201a64dd941e7ea6ea0187cf
Sachin Reddy <sachinreddy@sachins-macbook.attlocal.net>
2019-01-08 14:25:31 -0500

    Added timeout for API Sync and updated GUI.

--- application.py
+++ application.py
@@ -13,9 +13,13 @@ import time
 import todoist_scheduler
 import getpass
 from globs import *
+import signal
 
 api = todoist.TodoistAPI()
 q = queue.LifoQueue()
+
+def handler(signum, frame):
+    raise Exception
 
 class Application:
     def __init__ (self):
@@ -34,36 +38,59 @@ class Application:
         self.tasks = []
         self.curr_task_num = None
         self.curr_task = None
-        self.curr_item = None
         
-        # are these necessary?
+        # necessary?
         self.task_blocks = 0
         self.task_break_blocks = 0
         
         self.goal_hrs = 0
         self.goal_blocks = 0
+        self.sync_status = None
     
     def get_todays_tasks(self):
-        time.sleep(1)
-        api.user.login(self.user, self.password)
-        time.sleep(1)
-        response = api.sync()
-        time.sleep(1)
-        today = datetime.now(timezone('EST')).strftime("%a %d %b")
-        for item in response['items']:
-            due = item['due_date_utc']
-            if due:
-                due_est = self.datetimeConverter(due)
-                if due_est == today:
-                    self.items.append(item)
-    
+        tasks = []
+        response = None
+        items = None
+        while not response:
+            try:
+                signal.signal(signal.SIGALRM, handler)
+                signal.alarm(2)
+                response = api.sync()
+                items = response['items']
+                signal.alarm(0)
+            except:
+                signal.alarm(0)
+        if items:
+            today = datetime.now(timezone('EST')).strftime("%a %d %b")
+            for item in items:
+                due = item['due_date_utc']
+                if due:
+                    due_est = self.datetimeConverter(due)
+                    if due_est == today:
+                        tasks.append(item)
+        return tasks
+
     def getTasks(self):
-        self.get_todays_tasks()
-        if not self.items:
-            return
-        d = self.task_formatter(self.items)
-        self.tasks = sorted(d.items(), key=operator.itemgetter(1), reverse=False)
-        self.num_tasks = len(self.tasks)
+        t = []
+        f = False
+        l = self.get_todays_tasks()
+        if l:
+            self.items = l
+            d = self.task_formatter(self.items)
+            t = sorted(d.items(), key=operator.itemgetter(1), reverse=False)
+            return t
+
+    def userLogin(self):
+        f = False
+        while not f:
+            try:
+                signal.signal(signal.SIGALRM, handler)
+                signal.alarm(3)
+                api.user.login(self.user, self.password)
+                signal.alarm(0)
+                f = True
+            except:
+                signal.alarm(0)
 
     def datetimeConverter(self, due):
         from_zone = tz.tzutc()
@@ -89,7 +116,9 @@ class Application:
 
     def drawMonitor(self, stdscr):
         global q
+        
         stdscr.clear()
+        stdscr.refresh()
         t = 0
         b = 0
         k=[]
@@ -119,8 +148,6 @@ class Application:
                 currTask = "{}".format(self.curr_task[0][1])
             
             upNext = "Up Next:"[:width-1]
-            nextTasks = self.tasks
-            
             help = "Help:"[:width-1]
             help_1 = "-> start: s [task #]"[:width-1]
             help_2 = "-> pause/unpause: p"[:width-1]
@@ -144,7 +171,7 @@ class Application:
             
             tsec, sec = divmod(self.total_time, 60)
             hr, min = divmod(tsec, 60)
-            totalTime = "Working Time: %d:%02d:%02d" % (hr, min, sec)
+            totalTime = "Working: %d:%02d:%02d" % (hr, min, sec)
             
             tsec_, sec_ = divmod(self.break_time, 60)
             hr_, min_ = divmod(tsec_, 60)
@@ -181,31 +208,29 @@ class Application:
                 v_ = chr(v)
                 if v_ in LETTERS or v_ == '\n' or v_.isdigit():
                     k.append(v_)
-                        
             if k:
                 if k[-1] == '\n':
+                    self.sync_status = None
                     k=[]
                 elif k[0] == 'q':
                     stdscr.clear()
                     return
-                arg = " ".join(k)
+                elif k[0] == 'g':
+                    stdscr.clear()
+                    return
+                arg = "".join(k)
             
-            # Render the status bar
             stdscr.attron(curses.color_pair(3))
             stdscr.addstr(height-1, 0, statusbarstr)
             stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
             stdscr.attroff(curses.color_pair(3))
             
-            # Turning on attributes for title
             stdscr.attron(curses.color_pair(2))
             stdscr.attron(curses.A_BOLD)
-            # Rendering title
             stdscr.addstr(start_y, start_x_title, title)
-            # Turning off attributes for title
             stdscr.attroff(curses.color_pair(2))
             stdscr.attroff(curses.A_BOLD)
             
-            # Print rest of text
             stdscr.attron(curses.A_BOLD)
             stdscr.addstr(start_y + 1, 0, now)
             stdscr.attroff(curses.A_BOLD)
@@ -216,7 +241,7 @@ class Application:
             stdscr.addstr(start_y + 4, 0, upNext)
             stdscr.attroff(curses.A_BOLD)
             
-            for idx, i in enumerate(nextTasks):
+            for idx, i in enumerate(self.tasks):
                 if i == self.curr_task:
                     stdscr.attron(curses.color_pair(4))
                     stdscr.attron(curses.A_BOLD)
@@ -225,7 +250,7 @@ class Application:
                     stdscr.attroff(curses.A_BOLD)
                 else:
                     stdscr.addstr(start_y + 5 + idx, 0, "{}. {}".format(idx+1, i[0][1]))
-            offset = start_y + 5 + len(nextTasks)
+            offset = start_y + 5 + len(self.tasks)
             
             stdscr.attron(curses.A_BOLD)
             stdscr.addstr(offset+1, 0, help)
@@ -275,19 +300,16 @@ class Application:
             
             iput = "--> {}".format(arg)
             stdscr.addstr(height-3, 0, iput)
+            if self.sync_status:
+                stdscr.addstr(height-3, width-len(self.sync_status)-1, self.sync_status)
             stdscr.move(height-3, len(iput))
             
             if arg:
-                if arg.startswith('g'):
-                    self.getTasks()
-                    k=[]
-                    arg = ''
-                elif len(k) == 2 and arg.startswith('s'):
+                if len(k) == 2 and arg.startswith('s'):
                     t = int(k[1])
                     if t - 1 >= 0 and t - 1 < len(self.tasks):
                         self.curr_task_num = t
                         self.curr_task = self.tasks[self.curr_task_num-1]
-                        self.curr_item = self.items[self.curr_task_num-1]
                         self.timer = timer.Timer()
                         k=[]
                         arg = ''
@@ -308,19 +330,17 @@ class Application:
                     arg = ''
                 elif arg.startswith('d'):
                     if not self.started:
-                        error_msg = "[ERROR TIMER] No task to complete."
-                        stdscr.addstr(height - 5, 0, error_msg)
-                        continue
-                    del self.tasks[self.curr_task_num-1]
-                    self.completeItem()
-                    self.curr_task_num = None
-                    self.curr_task = None
-                    time_, blocks_ = self.timer.stop()
-                    self.total_time += self.getTotalSeconds(time_)
-                    self.total_blocks += blocks_
-                    self.started = False
-                    k=[]
-                    arg = ''
+                        k=[]
+                        arg = ''
+                        pass
+                    else:
+                        self.completeItem()
+                        time_, blocks_ = self.timer.stop()
+                        self.total_time += self.getTotalSeconds(time_)
+                        self.total_blocks += blocks_
+                        self.started = False
+                        k=[]
+                        arg = ''
                 elif arg.startswith('t'):
                     self.clearCache()
                     k=[]
@@ -341,13 +361,21 @@ class Application:
                 k_ = chr(k)
                 if k_ in LETTERS or k_ == '\n' or k_.isdigit():
                     q.put(k)
+                    if k_ == 'g':
+                        return False
                     if k_ == 'q':
-                        return
+                        return True
 
     def completeItem(self):
-        item = api.items.get_by_id(self.curr_task[0][0])
-        item.complete()
-        api.commit()
+        item = None
+        while not item:
+            item = api.items.get_by_id(self.curr_task[0][0])
+            item.complete()
+            api.commit()
+        del self.tasks[self.curr_task_num-1]
+        self.curr_task_num = None
+        self.curr_task = None
+
 
     def clearCache(self):
         if os.path.exists(todoist_scheduler.pickle_path):
@@ -363,18 +391,35 @@ class Application:
             self.password = getpass.getpass('Password: ')
 
         if not self.started:
-            self.getTasks()
+            self.userLogin()
+            self.tasks = self.getTasks()
+            self.num_tasks = len(self.tasks)
 
         if not self.goal_hrs and not self.goal_blocks:
             self.goal_hrs = int(input('Estimated # of hours:'))
             self.goal_blocks = self.goal_hrs * int(MIN_IN_HR / BLOCK_LEN)
-        
-        t1 = threading.Thread(target=curses.wrapper, args=(self.inputting,))
-        t2 = threading.Thread(target=curses.wrapper, args=(self.drawMonitor,))
-        t1.start()
-        t2.start()
-        t1.join()
-        t2.join()
+
+        f = False
+        while not f:
+            t1 = threading.Thread(target=curses.wrapper, args=(self.drawMonitor,))
+            t1.start()
+            f = curses.wrapper(self.inputting)
+            t1.join()
+            if not f:
+                l = self.getTasks()
+                if l:
+                    print('got new tasks')
+                    self.sync_status = "+ {} tasks.".format(len(l))
+                    if self.tasks:
+                        for i in l:
+                            self.tasks.append(i)
+                            self.num_tasks = len(self.tasks)
+                    else:
+                        self.tasks = l
+                        self.num_tasks = len(self.tasks)
+                else:
+                    print('No new tasks.')
+                    self.sync_status = "No new tasks."
 
     def getTotalSeconds(self, s):
         tp=s.split(':')
